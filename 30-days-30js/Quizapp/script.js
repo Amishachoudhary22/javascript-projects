65e982c6862674ff86a9e77f1e4607e6d2eea5c1
Amisha Choudhary <103454929+Amishachoudhary22@users.noreply.github.com>
2024-10-26 19:33:28 +0530

    Add files via upload

---
+++ 30-days-30js/Quizapp/script.js
@@ -0,0 +1,38 @@
+const questions =[
+    {
+        questions : "which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "giraffe", correct: false},
+        ]
+    }
+    {
+        questions : "which is the largest chocolate in the world?",
+        answers: [
+            { text: "dairy milk", correct: false },
+            { text: "Aditya", correct: true},
+            { text: "kitkat", correct: false},
+            { text: "amul 90% bitter", correct: false},
+        ]
+    }
+    {
+        questions : "which is the hotest girl in the world for aditya?",
+        answers: [
+            { text: "jahnvi kapoor", correct: false },
+            { text: "Rachel in suits", correct: false},
+            { text: "Amisha", correct: true},
+            { text: "Ester Expośito", correct: false},
+        ]
+    }
+    {
+        questions : "which is the largest animal in the world?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "giraffe", correct: false},
+        ]
+    }
+]
